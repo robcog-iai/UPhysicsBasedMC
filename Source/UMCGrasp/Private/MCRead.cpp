f8ea7286dd5216b956901272189ce13d36ef7b11
Johannes Bunk <bunk@uni-bremen.de>
2019-03-11 00:14:20 +0100

    added first core classes

---
+++ Source/UMCGrasp/Private/MCRead.cpp
@@ -0,0 +1,85 @@
+// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+
+#include "MCRead.h"
+#include "ConfigCacheIni.h"
+#include "Runtime/CoreUObject/Public/UObject/Package.h"
+#include "Runtime/AssetRegistry/Public/AssetRegistryModule.h"
+#include "Runtime/Engine/Classes/Engine/ObjectLibrary.h"
+
+FMCAnimationData UMCRead::ReadFile(const FString& SkeletalMeshName, const FString& Name)
+{
+	FMCAnimationData DataStruct = FMCAnimationData();
+
+	for (UMCGraspDataAsset* DataAsset : LoadAllAssets())
+	{
+		if (DataAsset->SkeletalName == SkeletalMeshName && DataAsset->AnimationName == Name)
+		{
+			DataStruct.AnimationName = DataAsset->AnimationName;
+			DataStruct.SkeletalName = DataAsset->SkeletalName;
+			for (FString BoneName : DataAsset->BoneNames)
+			{
+				DataStruct.BoneNames.Add(BoneName);
+			}
+			for (FMCEpisodeData Episode : DataAsset->PositionEpisode)
+			{
+				DataStruct.AddNewPositionData(Episode);
+			}
+			break;
+		}
+	}
+
+	return DataStruct;
+}
+
+TArray<FString> UMCRead::ReadNames(const FString& SkeletalMeshName)
+{
+	TArray<FString> Files;
+
+	TArray<UMCGraspDataAsset*> DataAssets = LoadAllAssets();
+
+	for(UMCGraspDataAsset* DataAsset : DataAssets)
+	{
+		if (DataAsset->SkeletalName == SkeletalMeshName)
+		{
+			Files.Add(DataAsset->AnimationName);
+		}
+	}
+
+	return Files;
+}
+
+TArray<UMCGraspDataAsset*> UMCRead::LoadAllAssets()
+{
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(FName("AssetRegistry"));
+	IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+	while(AssetRegistry.IsLoadingAssets()) 
+	{
+	}
+
+	return OnRegistryLoaded();
+}
+
+TArray<UMCGraspDataAsset*> UMCRead::OnRegistryLoaded()
+{
+	FAssetRegistryModule& AssetRegistryModule = FModuleManager::LoadModuleChecked<FAssetRegistryModule>(FName("AssetRegistry"));
+	IAssetRegistry& AssetRegistry = AssetRegistryModule.Get();
+
+	FARFilter Filter;
+	Filter.ClassNames = { TEXT("MCGraspDataAsset")};
+	Filter.PackagePaths.Add("/URealisticGrasping/Grasps");
+
+	TArray<FAssetData> AssetList;
+
+	AssetRegistry.GetAssets(Filter, AssetList);
+
+	TArray<UMCGraspDataAsset*> GraspAssets;
+
+	for (const FAssetData& DataAsset : AssetList) {
+		UObject* Obj = DataAsset.GetAsset();
+		if (Obj->GetClass()->IsChildOf(UMCGraspDataAsset::StaticClass())) {
+			GraspAssets.Add(Cast<UMCGraspDataAsset>(Obj));
+		}
+	}
+
+	return GraspAssets;
+}
---
+++ Source/UMCGrasp/Public/MCRead.h
@@ -0,0 +1,38 @@
+// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+
+#pragma once
+
+#include "MCAnimationDataStructure.h"
+#include "Runtime/Core/Public/HAL/PlatformFilemanager.h"
+#include "Runtime/Core/Public/Misc/LocalTimestampDirectoryVisitor.h"
+#include "CoreMinimal.h"
+#include "MCGraspDataAsset.h"
+
+/**
+ * This Class is used to read animations into files
+ */
+class UMCGRASP_API UMCRead
+{
+public:
+	/*
+	This function reads a ini file. It should be located in: GameDir/GraspAnimations/SkeletalMeshName/Name.ini
+	@param SkeletalMeshName, this represents the directory
+	@param Name, the file name in this directory
+	@return the animation 
+	*/
+	static FMCAnimationData ReadFile(const FString& SkeletalMeshName, const FString& Name);
+
+	/*
+	This function reads all posibility animations for one skeleton 
+	(e.g it reads all animations in GameDir/GraspAnimations/SkeletalMeshName/...)
+	@param SkeletalMeshName, the directory that should be searched
+	@return all animations
+	*/
+	static TArray<FString> ReadNames(const FString& SkeletalMeshName);
+
+	static TArray<UMCGraspDataAsset*> LoadAllAssets();
+
+private:
+
+	static TArray<UMCGraspDataAsset*> OnRegistryLoaded();
+};
