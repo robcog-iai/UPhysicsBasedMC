25050357644c11b2af41fb9b0071b21829e3080a
Johannes Bunk <bunk@uni-bremen.de>
2019-03-13 02:37:33 +0100

    updated version of RealisticGraspingController

---
+++ Source/UMCGrasp/Private/MCGraspingController.cpp
@@ -0,0 +1,180 @@
+// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+
+#include "MCGraspingController.h"
+
+
+// Sets default values for this component's properties
+UMCGraspingController::UMCGraspingController()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMCGraspingController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (ASkeletalMeshActor* TempHand = Cast<ASkeletalMeshActor>(GetOwner()))
+	{
+		Hand = TempHand;
+		USkeletalMeshComponent* const SkelComp = Hand->GetSkeletalMeshComponent();
+		SkelComp->SetSimulatePhysics(true);
+		SkelComp->SetEnableGravity(false);
+
+
+		float SpringTemp = 9000.0f;
+		float DampingTemp = 1000.0f;
+		float ForceLimitTemp = 0.0f;
+		if (SkelComp->GetPhysicsAsset())
+		{
+			// Hand joint velocity drive
+			SkelComp->SetAllMotorsAngularPositionDrive(true, true);
+
+			// Set drive parameters
+			SkelComp->SetAllMotorsAngularDriveParams(SpringTemp, DampingTemp, ForceLimitTemp);
+		}
+	}
+}
+
+
+// Called every frame
+void UMCGraspingController::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UMCGraspingController::UpdateGrasp(const float Input)
+{
+
+	if (GraspingData.GetNumberOfEpisodes() <= 1)
+	{
+		return;
+	}
+
+	Spring = 9000 * (1 + Input);
+
+	// Checks if input is relevant
+	if (Input > 0.001)
+	{
+		if (!bIsGrasping) {
+			bIsGrasping = true;
+		}
+
+		// Calculations for moving in multiple steps
+		// StepSize defines how much of the 0-1 input is between each step
+		// For 3 Steps for example there is a input of 0,5 seperating each 
+		float StepSize = 1 / ((float)GraspingData.GetNumberOfEpisodes() - 1);
+		// Calculates how many steps we have passed, given then current input
+		// When rounded down we know which step came before with this input
+		float StepIteratorCountFloat = Input / StepSize;
+
+		// We will be rounding down and a input of exactly 1 causes problems there
+		if (Input >= 1)
+		{
+			StepIteratorCountFloat = 0.999999 / StepSize;
+		}
+
+		// Uses the float to int cast to round down
+		int StepIteratorCountInt = (int)StepIteratorCountFloat;
+
+		// We calculate how far the input is past the step that came before it
+		float NewInput = StepIteratorCountFloat - (float)StepIteratorCountInt;
+
+		// Manipulate Orientation Drives
+		FMCEpisodeData TargetOrientation;
+		LerpHandOrientation(&TargetOrientation, GraspingData.GetPositionDataWithIndex(StepIteratorCountInt), GraspingData.GetPositionDataWithIndex(StepIteratorCountInt + 1), NewInput);
+		DriveToHandOrientationTarget(&TargetOrientation);
+
+	}
+	else {
+		if (bIsGrasping)
+		{
+			// Stop grasping
+			StopGrasping();
+		}
+	}
+}
+
+void UMCGraspingController::StopGrasping()
+{
+	// Stop Grasp
+	FMCEpisodeData Save = GraspingData.GetPositionDataWithIndex(0);
+	DriveToHandOrientationTarget(&Save);
+	if (bIsInQueue)
+	{
+		GraspingData = GraspQueue;
+		bIsInQueue = false;
+	}
+	bIsGrasping = false;
+}
+
+void UMCGraspingController::LerpHandOrientation(FMCEpisodeData* Target, FMCEpisodeData Initial, FMCEpisodeData Closed, const float Input)
+{
+	TArray<FString> TempArray;
+	Initial.GetMap()->GenerateKeyArray(TempArray);
+	for (FString s : TempArray) {
+		Target->AddNewBoneData(s, FMath::LerpRange(
+			Initial.GetBoneData(s)->AngularDriveInput,
+			Closed.GetBoneData(s)->AngularDriveInput, Input));
+	}
+}
+
+void UMCGraspingController::DriveToHandOrientationTarget(FMCEpisodeData* Target) 
+{
+	FConstraintInstance* Constraint = nullptr;
+	TArray<FString> TempArray;
+	Target->GetMap()->GenerateKeyArray(TempArray);
+	for (FString s : TempArray) {
+		Constraint = BoneNameToConstraint(s);
+		if (Constraint) {
+			Constraint->SetAngularOrientationTarget(Target->GetMap()->Find(s)->AngularDriveInput.Quaternion());
+		}
+	}
+}
+
+FConstraintInstance* UMCGraspingController::BoneNameToConstraint(FString BoneName) {
+	FConstraintInstance* Constraint = nullptr;
+	UActorComponent* component = Hand->GetComponentByClass(USkeletalMeshComponent::StaticClass());
+	USkeletalMeshComponent* skeletalComponent = Cast<USkeletalMeshComponent>(component);
+
+	//sets up the constraints so they can be moved 
+	if (!bBonesConstraintsSetUp) {
+		for (FConstraintInstance* NewConstraint : skeletalComponent->Constraints) {
+			if (NewConstraint->ConstraintBone1.ToString() == BoneName) {
+				NewConstraint->SetOrientationDriveTwistAndSwing(false, false);
+				NewConstraint->SetAngularVelocityDriveTwistAndSwing(false, false);
+				NewConstraint->SetAngularVelocityDriveSLERP(false);
+				NewConstraint->SetOrientationDriveSLERP(true);
+				NewConstraint->SetAngularDriveParams(Spring, 1000, 0);
+			}
+		}
+		bBonesConstraintsSetUp = true;
+	}
+
+	//finds the constraint responsible for moving this bone
+	for (FConstraintInstance* NewConstraint : skeletalComponent->Constraints) 
+	{
+		NewConstraint->SetAngularDriveParams(Spring, 1000, 0);
+		if (NewConstraint->ConstraintBone1.ToString() == BoneName) {
+			Constraint = NewConstraint;
+		}
+	}
+	return Constraint;
+}
+
+void UMCGraspingController::SetGraspingData(FMCAnimationData Data) {
+	// if player is grasping put new grasp in queue, else change grasp immediately
+	if (bIsGrasping)
+	{
+		GraspQueue = Data;
+		bIsInQueue = true;
+		return;
+	}
+	GraspingData = Data;
+}
---
+++ Source/UMCGrasp/Public/MCGraspingController.h
@@ -0,0 +1,95 @@
+// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+
+#pragma once
+
+#include "Engine.h"
+#include "Components/ActorComponent.h"
+#include "MCAnimationDataStructure.h"
+#include "Runtime/Engine/Classes/Animation/SkeletalMeshActor.h"
+#include "MCGraspingController.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UMCGRASP_API UMCGraspingController : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMCGraspingController();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/*
+	Loads the data of grasps into the grasping controller
+	@param Data - data to load
+	*/
+	UFUNCTION()
+	void SetGraspingData(FMCAnimationData Data);
+
+	/*
+	Responsible for updating the movement of all bones
+	@param Hand - the AGraspingHand to move
+	@param Input - number from 0-1 that indicates how far the grasping trigger is being pushed down
+	*/
+	void UpdateGrasp(const float Input);
+
+private:
+
+	/*
+	Calls LerpFingerOrientation on every finger
+	@param Target - where calculated positions are saved
+	@param Initial - initial hand state if input was 0
+	@param Closed - final hand state if input was 1 
+	@param Input - number from 0-1 that indicates how far the grasping trigger is being pushed down
+	*/
+	void LerpHandOrientation(FMCEpisodeData* Target, FMCEpisodeData Initial, FMCEpisodeData Closed, const float Input);
+
+	/*
+	Calls DriveToFingerOrientationTarget for every finger.
+	@param Target - the target position that has been calculated by lerp
+	@param Hand - the AGraspingHand to move
+	*/
+	void DriveToHandOrientationTarget(FMCEpisodeData* Target);
+
+	/*
+	Finds out which constraint belongs to which bone and sets them up  so we cann move them
+	@param BoneName - name of bone
+	@param Hand - AGraspingHand with the skeleton that has to be searched for the constraint
+	*/
+	FConstraintInstance* BoneNameToConstraint(FString BoneName);
+
+	/*
+	Stops the grasping process and resets the booleans that were changed
+	@param Hand - AGraspingHand that should stop grasping
+	*/
+	void StopGrasping();
+
+	// If hand is grasping
+	bool bIsGrasping;
+
+	// Current grasp loaded into hand
+	FMCAnimationData GraspingData;
+
+	// If bone constraints are set up the right way
+	bool bBonesConstraintsSetUp;
+
+	// Spring value to be used for constraints
+	// Makes angular motors more powerfull depending on how much the grasping button is pushed down
+	float Spring;
+
+	// When changing grasp type while grasping, the new grasp isn't applied immediately
+	// Instead it is saved in this variable and applied once the user stops grasping 
+	FMCAnimationData GraspQueue;
+
+	// Set to true when there is a grasp waiting to be applied
+	bool bIsInQueue;
+
+	ASkeletalMeshActor* Hand;
+};
