ef7cb05a7004a93161a330b32d7eb6cc296b6fb5
Andrei Haidu <andrei.haidu@yahoo.com>
2019-04-04 11:10:53 +0200

    added copyright year range

--- Source/UMCGraspEd/Private/MCGraspDataAssetFactory.cpp
+++ Source/UMCGraspEd/Private/MCGraspDataAssetFactory.cpp
@@ -1,4 +1,4 @@
-// Copyright 2018, Institute for Artificial Intelligence - University of Bremen
+// Copyright 2017-2019, Institute for Artificial Intelligence - University of Bremen
 
 #include "MCGraspDataAssetFactory.h"
 #include "Editor/UnrealEd/Public/Editor.h"
